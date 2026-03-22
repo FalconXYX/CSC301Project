19c1139bb93ab822afee0796a5c2638e1771f059
Jordan Jones <124713617+MarooJones@users.noreply.github.com>
2026-03-22 11:00:58 -0700

    3.4.3 Initial clinic values page (#138)

--- PhysioFind-FrontEnd/src/router/index.ts
+++ PhysioFind-FrontEnd/src/router/index.ts
@@ -14,6 +14,7 @@ import PrivacyPolicyPage from '@/pages/PrivacyPolicyPage.vue'
 import AboutPage from '@/pages/AboutPage.vue'
 import ClinicCreationPage from '@/pages/clinics/ClinicCreationPage.vue'
 import ClinicDashboardPage from '@/pages/clinics/ClinicDashboardPage.vue'
+import ClinicsValuePage from '@/pages/clinics/ClinicsValuePage.vue'
 
 import ErrorPage from '@/pages/error/ErrorPage.vue'
 import UserAppointmentsPage from '@/pages/appointments/UserAppointmentsPage.vue'
@@ -38,6 +39,10 @@ const router = createRouter({
         {
           path: 'patients',
           component: PatientsValuePage,
+        },
+        {
+          path: 'clinics',
+          component: ClinicsValuePage,
         },
         {
           path: 'privacy-policy',
