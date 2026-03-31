c36236d1383edfc0960df62222d9d55e552dcb17
Drudles <nerbascg@yahoo.ca>
2026-03-31 15:34:04 -0400

    fixed bcrypt import error

--- PhysioFind-Express/routes/users/createUser.js
+++ PhysioFind-Express/routes/users/createUser.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var prisma = require("../../config/prisma");
+var bcrypt = require('bcrypt');
 
 const { createClient } = require("@supabase/supabase-js");
 
